76ba694227e3ecbc6a4a209adc07865fa26b5309
LucasOliveira <lucas.barros1804sec@gmail.com>
2022-10-01 19:55:02 -0300

    Removing SocketIO

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express"),
       app = express(),
       http = require("http"),
       server = http.createServer(app),
-      ejs = require("ejs"),
-      io = require("socket.io")(server)
+      ejs = require("ejs")
 
 app.use(express.static(__dirname+"/src/"))
 app.set("views",__dirname+"/src/")
@@ -11,10 +10,6 @@ app.engine("html",ejs.renderFile)
 app.set("view engine", "html")
 app.get("/",(req,res)=>{
     res.render(__dirname+"/src/index.html")
-})
-
-io.on("connection",socket=>{
-    console.log(`Socket connected: ${socket.id}`)
 })
 
 server.listen(8080, ()=>{
